e75f9ca58170ed160d89ada19f4d8afa7d997e0b
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-05-29 17:08:46 +0530

    1st problem solved

---
+++ index.js
@@ -0,0 +1,12 @@
+//1st problem
+   // let clickBtn = document.getElementById('clickBtn');
+    // function btnClicked(){
+    //     if (clickBtn.innerText==="click me!") {
+    //         clickBtn.innerText = "clicked";
+    //     }
+    //     else{
+    //         clickBtn.innerText = "click me!";
+    //     }
+    // }
+//2nd problem
+    
